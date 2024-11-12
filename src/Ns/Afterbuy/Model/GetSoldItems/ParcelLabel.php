93e3e2ea2fb3f9f7f42a7f550948532bdcb370aa
Tibor Balogh <tibee@tibee.hu>
2024-11-12 09:43:21 +0100

    Create ParcelLabel.php

---
+++ src/Ns/Afterbuy/Model/GetSoldItems/ParcelLabel.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Ns\Afterbuy\Model\GetSoldItems;
+
+use JMS\Serializer\Annotation as Serializer;
+use Ns\Afterbuy\Model\AbstractModel;
+use \DateTime;
+use Ns\Afterbuy\Model\FloatType;
+
+/**
+ * Class SoldItem
+ */
+class ParcelLabel extends AbstractModel
+{
+
+    /**
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("ItemID")
+     * @var int
+     */
+    protected $itemId;
+    
+    /**
+     * @Serializer\Type("integer")
+     * @Serializer\SerializedName("PackageNumber")
+     * @var string
+     */
+    protected $packageNumber;
+
+    /**
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("ParcelLabelNumber")
+     * @var string
+     */
+    protected $parcelLabelNumber;
+
+    /**
+     * @return int
+     */
+    public function getItemId()
+    {
+        return $this->itemId;
+    }
+    
+    /**
+     * @return integer
+     */
+    public function getPackageNumber()
+    {
+        return $this->packageNumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParcelLabelNumber()
+    {
+        return $this->parcelLabelNumber;
+    }
+
+    /**
+     * @return ParcelLabel[]
+     */
+    public function getParcelLabels()
+    {
+        return $this->parcelLabels;
+    }
+    
+}
