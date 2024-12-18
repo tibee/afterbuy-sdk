252e44230f4fac3ce5482ba6520667c45d3609d1
Balogh Tibor <tibee@php.net>
2024-12-18 12:15:04 +0100

    setLogger signature fix #1

--- src/Ns/Afterbuy/Client/Request.php
+++ src/Ns/Afterbuy/Client/Request.php
@@ -126,7 +126,7 @@ class Request implements LoggerAwareInterface
      * @param LoggerInterface $logger
      * @return void
      */
-    public function setLogger(LoggerInterface $logger)
+    public function setLogger(LoggerInterface $logger):void
     {
         $this->logger = $logger;
     }
